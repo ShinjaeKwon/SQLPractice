ef0df12c37c6581eb0f1dcef1f986edc6601021f
JaeKwonShin <sjk6437@gmail.com>
2021-07-25 21:57:13 +0900

    SQLHW
    15주차 과제

---
+++ SQLHW/15-1주차 과제.sql
@@ -0,0 +1,22 @@
+--실습 1
+--Q1-1
+1. DATABASE
+2. SEOUL
+3. SALES
+4. CHICAGO
+--Q1-2
+행 상태, 한 쪽세션이 끝나지 않아 끝날때 까지 대기중
+--Q1-3
+30, DATABASE , SEOUL
+--Q1-4
+1. SALES
+2. CHICAGO
+3. DATABASE
+4. SEOUL
+--Q1-5
+1. DATABASE
+2. SEOUL
+3. DATABASE
+4. SEOUL
+
+SELECT * FROM DEPT;
